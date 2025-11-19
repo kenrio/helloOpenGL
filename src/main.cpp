ec114b5c8f3b747882afb7346b7d6662f8503487
kenrio <kenken7272721@gmail.com>
2025-11-20 02:12:13 +0900

    Reached to draw a triangle with OpenGL ;

--- src/main.cpp
+++ src/main.cpp
@@ -47,8 +47,15 @@ int	main(void)
 
 	unsigned int	VBO;
 	glGenBuffers(1, &VBO);
+
+	unsigned int	VAO;
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
 
 	const char	*vertexShaderSource = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
@@ -71,11 +78,11 @@ int	main(void)
 		std::cout << "Error: vertex shader compilation failed\n" << infoLog << std::endl;
 	}
 
-	const char	*fragmentShaderSource = "#verion 330 core\n"
+	const char	*fragmentShaderSource = "#version 330 core\n"
 		"out vec4 FragColor;\n"
 		"void main()\n"
 		"{\n"
-		"	FragColor = vec4(1.0f, 0.5.f, 0.2f, 1.0f);\n"
+		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 		"}\0";
 
 	unsigned int	fragmentShader;
@@ -89,6 +96,23 @@ int	main(void)
 		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
 		std::cout << "Error: fragment shader compilation failed\n" << infoLog << std::endl;
 	}
+
+	unsigned int	shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "Error: shader program link failed\n" << infoLog << std::endl;
+	}
+	
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 	
 	while (!glfwWindowShouldClose(window))
 	{
@@ -96,6 +120,10 @@ int	main(void)
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
